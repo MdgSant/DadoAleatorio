3a62594f4d4e1bb1847ec23314edf947fce749c1
MdgSant <Miguelssantanatbl@gmail.com>
2025-09-30 19:45:39 -0300

    Alterado pra uma versão mais avançada com menos linhas de código

---
+++ DadoAleatorio/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DadoAleatorio
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DadoAleatorio/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace DadoAleatorio
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Rolar(object sender, EventArgs e)
+        {
+            int selecaoLados = Convert.ToInt32(Nmpckr.SelectedItem);
+            Random selecionado = new Random();
+            int lados = selecionado.Next(1, selecaoLados + 1);
+            
+            Numerolbl.Text = lados.ToString();
+            DadoImagem.Source = $"lado{Nmpckr.SelectedItem}.png";
+
+        }
+    }
+
+}
